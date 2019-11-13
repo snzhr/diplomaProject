974f418e8422607d79341910a8119f3d7b6e5c44
snzhr <snzhrski@gmail.com>
2019-11-13 21:20:20 +0600

    Files uploaded

---
+++ gunicorn_config.py
@@ -0,0 +1,4 @@
+command = '/opt/django/webtechnologies/env/bin/gunicorn'
+pythonpath = '/opt/django/webtechnologies/webtech'
+bind = '127.0.0.1:8001'
+workers = 3
---
+++ webtech/gunicorn_config.py
@@ -0,0 +1,4 @@
+command = '/usr/home/django/webtechnologies/env/bin/gunicorn'
+pythonpath = '/usr/home/django/webtechnologies/webtech'
+bind = '127.0.0.1:8001'
+workers = 3
